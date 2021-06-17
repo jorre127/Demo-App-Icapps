eb0430d814a4fd0d38bb8a41338e28f9c42a500a
jordy <jordy.dj.jj@gmail.com>
2021-06-17 15:40:28 +0200

    Added authentication
    - Added login page with only a login button
    - Added authentication service that has a login function which executes a post request to the api.
    - Added a login function on the login page which uses the authentication service to log in and afterwards stores the resulting token in shared preferences.
    -Added a widget called AuthenticationWrapper which shows either the login page or the home page depending on if a token is stored.

--- icapps_app/lib/main.dart
+++ icapps_app/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:icapps_app/Pages/DetailPage/detailPage.dart';
 import 'package:icapps_app/Pages/HomePage/homePage.dart';
+import 'package:icapps_app/Pages/LoginPage/loginPage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,9 +13,41 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-      ),
-      home: HomePage(),
+      theme: ThemeData(),
+      home: AuthenticationWrapper(),
+      routes: {
+        "/Home": (_) => HomePage(),
+        "/Detail": (_) => DetailPage(),
+        "/Login": (_) => LoginPage(),
+      },
     );
+  }
+}
+
+class AuthenticationWrapper extends StatefulWidget {
+  const AuthenticationWrapper({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _AuthenticationWrapperState createState() => _AuthenticationWrapperState();
+}
+
+class _AuthenticationWrapperState extends State<AuthenticationWrapper> {
+  Widget currentPage = LoginPage();
+  void selectWidget(SharedPreferences sharedPreferences) {
+    setState(() {
+      if (sharedPreferences.getString("Token") != null) {
+        currentPage = HomePage();
+      } else {
+        currentPage = LoginPage();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SharedPreferences.getInstance().then((sharedPreferences) => selectWidget(sharedPreferences));
+    return currentPage;
   }
 }
